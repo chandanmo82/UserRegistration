798979fb5c54c7d85016e4385b7eff197dc71f1e
Chandan <chandanmohanty11111>
2021-10-18 19:07:23 +0530

    REFACTORED:Method to check for valid Mobile Number

--- src/com/company/UserRegistration.java
+++ src/com/company/UserRegistration.java
@@ -58,6 +58,23 @@ public class UserRegistration {
         else
             System.out.println(email + " is an Invalid Email address");
     }
+    public static void checkValidMobileNumber(String mobileNumber){
+
+        boolean isMobileNumber;
+        String mobileNumberRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+        Pattern patternObject = Pattern.compile(mobileNumberRegex);
+        if (mobileNumber == null) {
+            isMobileNumber = false;
+        }
+        Matcher matcherObject = patternObject.matcher(mobileNumber);
+        isMobileNumber =  matcherObject.matches();
+
+        if(isMobileNumber)
+            System.out.println(mobileNumber+" is a Valid  Mobile Number\n");
+        else
+            System.out.println(mobileNumber+" is an Invalid Mobile Number");
+    }
+
 
     public static void main(String[] args) {
 
@@ -76,6 +93,10 @@ public class UserRegistration {
         System.out.println("Enter Your Email Address");
         String email = scannerObject.next();
         checkValidEmail(email);
+
+        System.out.println("Enter Your Mobile Number");
+        String mobileNumber = scannerObject.next();
+        checkValidMobileNumber(mobileNumber);
 
         scannerObject.close();
 
